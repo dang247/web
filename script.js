598540157176bac963e077e8d447fd9ed9aae8b6
Ngô Hải Đăng <dangpro362@gmail.com>
2026-09-14 21:20:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const passwordInput = document.getElementById('password-input');
+const goBtn = document.getElementById('go-btn');
+const secretContent = document.getElementById('secret-content');
+const secretPassword = "12345"; 
 
+goBtn.addEventListener('click', function() {
+    if (passwordInput.value === secretPassword) {
+        passwordInput.style.display = 'none';
+        goBtn.style.display = 'none';
+        
+        secretContent.style.display = 'block';
+    } else {
+        alert("Sai rồi nhé!");
+    }
+});
